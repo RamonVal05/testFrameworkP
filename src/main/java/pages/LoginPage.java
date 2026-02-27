9816d75c5a7b9cef1a4224587909748c7b3d1ef0
Ramon <ramonvaldezcortez943@gmail.com>
2026-02-27 15:19:20 -0700

    Creation of methods to verify login errors

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -9,6 +9,7 @@ public class LoginPage {
     private By usernameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By loginButton = By.id("login-button");
+    private By epicSadface = By.xpath("//*[@id=\"login_button_container\"]/div/form/div[3]/h3/text()");
 
     public LoginPage(WebDriver driver){
         this.driver = driver;
@@ -26,10 +27,19 @@ public class LoginPage {
         driver.findElement(loginButton).click();
     }
 
+    public void verifyLoginDetails(){driver.findElement(epicSadface).equals("Epic sadface: Username and password do not match any user in this service");}
+
     public void login(String username, String password){
         enterPassword(username);
         enterPassword(password);
         clickLogin();
+    }
+
+    public void notlogin(String username, String password){
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+        verifyLoginDetails();
     }
 }
 
